945baa5f9d30c7d4a27ef93f0756b65e00d8ef91
Z-T440P <godz1024@outlook.com>
2019-08-05 05:15:19 +0800

    update index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,10 +15,7 @@ namespace AdminTemplate.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
